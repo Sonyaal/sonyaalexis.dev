429add1275de1c5282c82a8d24b4c1ee3d0059ad
Sonya Alexis <salexis@usc.edu>
2025-01-29 11:36:21 -0800

    Adding portfolio source

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,9 +8,20 @@ function Footer() {
   return (
     <footer>
       <div>
-        <a href="https://github.com/Sonyaal" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-        <a href="https://www.linkedin.com/in/sonya-alexis/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
-        <a href="mailto:salexis@usc.edu"><MailIcon/></a>
+        <a href="https://github.com/Sonyaal" target="_blank" rel="noreferrer">
+          <GitHubIcon />
+        </a>
+        <a href="https://www.linkedin.com/in/sonya-alexis/" target="_blank" rel="noreferrer">
+          <LinkedInIcon />
+        </a>
+        <a href="mailto:salexis@usc.edu">
+          <MailIcon />
+        </a>
+      </div>
+      <div className="view-source">
+        <a href="https://github.com/Sonyaal/sonya.alexis" target="_blank" rel="noreferrer">
+          View Source on GitHub
+        </a>
       </div>
     </footer>
   );
