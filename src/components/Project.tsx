130d6210c1ba0426a35c379cf2066b15e8ef3d27
Sonya Alexis <salexis@usc.edu>
2025-01-29 14:06:48 -0800

    updating projects section

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import project1 from '../assets/images/project1.png';
 import mock09 from '../assets/images/mock09.png';
 import '../assets/styles/Project.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRocket } from '@fortawesome/free-solid-svg-icons'; 
+import { faSoccerBall } from '@fortawesome/free-solid-svg-icons';
+import { faLaptopCode } from '@fortawesome/free-solid-svg-icons';
+import { faHeartbeat } from '@fortawesome/free-solid-svg-icons';
 
 function Project() {
     return(
@@ -9,10 +14,43 @@ function Project() {
         <h1>Technical Projects</h1>
         <div className="projects-grid">
             <div className="project">
-                <a href="https://github.com/uscmakers/BabyRobokeeper" target="_blank" rel="noreferrer"><img src={project1} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://github.com/uscmakers/BabyRobokeeper" target="_blank" rel="noreferrer"><h2>(Baby) Robokeeper</h2></a>
-                <p>Designed and built a “Robokeeper” to block shots in a foosball table in real time, utilizing CV. Specialized in Embedded
-                Systems.</p>
+                <a href="https://github.com/uscmakers/BabyRobokeeper" target="_blank" rel="noreferrer">
+                    <div className="project-box">
+                        <div className="project-icon">
+                            <FontAwesomeIcon icon={faSoccerBall} size="2x" />
+                        </div>
+                        <div className="project-details">
+                            <h2>(Baby) Robokeeper</h2>
+                            <p>Designed and built a “Robokeeper” to block shots in a foosball table in real time, utilizing CV. Specialized in Embedded Systems.</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div className="project">
+                {/* <Link to="/project/visual-regression-testing" className="project-link"> */}
+                    <div className="project-box">
+                        <div className="project-icon">
+                            <FontAwesomeIcon icon={faLaptopCode} size="2x" />
+                        </div>
+                        <div className="project-details">
+                            <h2>Visual Regression Testing</h2>
+                            <p>Developed a Visual Regression Testing Library for pixel-by-pixel comparison of widgets in the CI pipeline for PRs.</p>
+                        </div>
+                    </div>
+                {/* </Link> */}
+            </div>
+            <div className="project">
+                <a href="https://drive.google.com/file/d/1g_TSxvrwkMKgz2SCPKAuUY68HhRu5xX-/view?usp=sharing" target="_blank" rel="noreferrer">
+                    <div className="project-box">
+                        <div className="project-icon">
+                            <FontAwesomeIcon icon={faHeartbeat} size="2x" />
+                        </div>
+                        <div className="project-details">
+                            <h2>Graphene-Based Pressure Sensor</h2>
+                            <p>Utilizing a two-dimensional carbon-based nanomaterial to display the onset of SIDS.</p>
+                        </div>
+                    </div>
+                </a>
             </div>
         </div>
     </div>
