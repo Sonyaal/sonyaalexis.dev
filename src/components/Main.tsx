f8459871baececa1219bdd931c2db6f4da323472
Sonya Alexis <salexis@usc.edu>
2025-01-29 12:42:34 -0800

    adding gmail icon to header

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import MailIcon from '@mui/icons-material/Mail';
 import '../assets/styles/Main.scss';
 import headshot from '../assets/images/headshot.png';
 
@@ -66,12 +67,14 @@ function Main() {
           <div className="social_icons">
             <a href="https://github.com/Sonyaal" target="_blank" rel="noreferrer"><GitHubIcon /></a>
             <a href="https://www.linkedin.com/in/sonya-alexis/" target="_blank" rel="noreferrer"><LinkedInIcon /></a>
+            <a href="mailto:salexis@usc.edu"><MailIcon /></a>
           </div>
           <h1>Sonya Alexis</h1>
           <p>{displayText}</p>
           <div className="mobile_social_icons">
             <a href="https://github.com/Sonyaal" target="_blank" rel="noreferrer"><GitHubIcon /></a>
             <a href="https://www.linkedin.com/in/sonya-alexis/" target="_blank" rel="noreferrer"><LinkedInIcon /></a>
+            <a href="mailto:salexis@usc.edu"><MailIcon /></a>
           </div>
         </div>
       </div>
