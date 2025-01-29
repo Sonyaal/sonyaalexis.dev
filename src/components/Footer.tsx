1d756d20cf10f8349d32f5488dd63a89d8c7ea69
Sonya Alexis <salexis@usc.edu>
2025-01-29 15:52:14 -0800

    adding footer section background

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,26 +2,28 @@ import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import MailIcon from '@mui/icons-material/Mail';
-import '../assets/styles/Footer.scss'
+import '../assets/styles/Footer.scss';
 
 function Footer() {
   return (
     <footer>
-      <div>
-        <a href="https://github.com/Sonyaal" target="_blank" rel="noreferrer">
-          <GitHubIcon />
-        </a>
-        <a href="https://www.linkedin.com/in/sonya-alexis/" target="_blank" rel="noreferrer">
-          <LinkedInIcon />
-        </a>
-        <a href="mailto:salexis@usc.edu">
-          <MailIcon />
-        </a>
-      </div>
-      <div className="view-source">
-        <a href="https://github.com/Sonyaal/sonya.alexis" target="_blank" rel="noreferrer">
-          View Source on GitHub
-        </a>
+      <div className="footer-content">
+        <div className="social-links">
+          <a href="https://github.com/Sonyaal" target="_blank" rel="noreferrer">
+            <GitHubIcon />
+          </a>
+          <a href="https://www.linkedin.com/in/sonya-alexis/" target="_blank" rel="noreferrer">
+            <LinkedInIcon />
+          </a>
+          <a href="mailto:salexis@usc.edu">
+            <MailIcon />
+          </a>
+        </div>
+        <div className="view-source">
+          <a href="https://github.com/Sonyaal/sonya.alexis" target="_blank" rel="noreferrer">
+            View Source on GitHub
+          </a>
+        </div>
       </div>
     </footer>
   );
