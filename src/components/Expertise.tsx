7093a3d5efd70b3d93a6b787df84f88a0d8e73ac
Sonya Alexis <salexis@usc.edu>
2025-01-30 13:34:32 -0800

    updating expertise desc

--- src/components/Expertise.tsx
+++ src/components/Expertise.tsx
@@ -45,7 +45,7 @@ function Expertise() {
                 <div className="skill">
                     <FontAwesomeIcon icon={faPython} size="3x"/>
                     <h3>Software Development</h3>
-                    <p>I work with a diverse array of languages from Python to C++ for backend development and utilize modern technologies such as React and Flask for frontend development.</p>
+                    <p>I work with a diverse languages from Python to C++ for backend development. I also utilize modern technologies such as React and Flask for frontend development.</p>
                     <div className="flex-chips">
                         <span className="chip-title">Tech stack:</span>
                         {labelsFirst.map((label, index) => (
@@ -69,7 +69,7 @@ function Expertise() {
                 <div className="skill">
                     <FontAwesomeIcon icon={faRaspberryPi} size="3x"/>
                     <h3>Embedded Systems</h3>
-                    <p>I have hands-on experience with embedded systems development, working with Raspberry Pi, Arduino, and other hardware platforms to design and implement integrated solutions.</p>
+                    <p>I have experience with embedded systems development, working hands-on with Raspberry Pi, Arduino, and other hardware platforms to design and implement integrated solutions.</p>
                     <div className="flex-chips">
                         <span className="chip-title">Tech stack:</span>
                         {labelsThird.map((label, index) => (
